272d93c9f2851d601a222c92c0efec50a24c402d
aizhanti <a.t.imankulova@gmail.com>
2020-04-24 10:50:00 +0900

    Done until channel_messages
    Co-authored-by: jbentleyjp <jbentleyjp@users.noreply.github.com>

--- models/channelMessages/create.js
+++ models/channelMessages/create.js
@@ -1,34 +1,23 @@
 module.exports = (knex, channelMessage) => {
   return async (params) => {
-    let keys = await knex("users")
-      .select()
-      .innerJoin("channel_messages", "users.id", "channel_messages.from_id")
-      .innerJoin("channels", "channels.id", "channel_messages.channel_id");
-    console.log(keys);
-
     await knex("channel_messages").insert({
       from_id: params.fromId,
       channel_id: params.channelId,
       message: params.message,
     });
-    const allMSG = await knex("channel_messages")
-      .where({
-        from_id: params.fromId,
-        channel_id: params.channelId,
-        message: params.message,
-      })
-      .select();
-    console.log("allMSG", allMSG[0]);
-    return allMSG.map((mess) => {
-      const smth = new channelMessage(mess);
-      console.log(smth, "IM SMTH");
-      return {
-        fromUser: smth.serialize().fromUser,
-        toChannel: smth.serialize().toChannel,
-        message: smth.serialize().message,
-      };
+    const keys = await knex("channel_messages")
+      .innerJoin("users", "users.id", "channel_messages.from_id")
+      .innerJoin("channels", "channels.id", "channel_messages.channel_id")
+      .select(
+        "channel_messages.id",
+        "users.username as from",
+        "channels.name as to",
+        "channel_messages.message as message",
+        "channel_messages.sent_at as sent_at"
+      );
+    return keys.map((mess) => {
+      console.log("mess.username", mess.username);
+      return new channelMessage(mess);
     });
-    // console.log(messages, "IM ALL THE MESSAGES")
-    // return { messages };
   };
 };
